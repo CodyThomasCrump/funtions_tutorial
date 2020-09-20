df9407d12db2ed58be26aaa20b99e0dc31acdb00
Cody Crump <codytcrump@gmail.com>
2020-09-20 11:49:55 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,41 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+result = area(7, 3)
+print(result)
+result = area(102, 57)
+print(result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(8, 6)
+print(result)
+
+result = subtract(4, 6)
+print(result)
+
+def add(num1, num2):
+    result = num1 + num2
+    return result 
+
+result = add(5, 7)
+print(result)
+
+result = add(10, 9)
+print(result)
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+result = multiply(10, 5)
+print(result)
+
+result = multiply(13, 7)
+print(result)
